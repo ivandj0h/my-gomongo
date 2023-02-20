b11d39979e883122fc5c5df62727a94ad57341f7
ivandjoh <ivandi.djoh@gmail.com>
2023-02-20 21:51:53 +0800

    added handleFunc

--- main.go
+++ main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+)
+
+type Item struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	Price int                `json:"price,omitempty" bson:"price,omitempty"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(context.Background())
+
+	itemsCollection := client.Database("mygomongo").Collection("items")
+
+	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			// Get all items
+			cur, err := itemsCollection.Find(context.Background(), primitive.D{})
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer cur.Close(context.Background())
+			var items []Item
+			for cur.Next(context.Background()) {
+				var item Item
+				if err := cur.Decode(&item); err != nil {
+					log.Println(err)
+					continue
+				}
+				items = append(items, item)
+			}
+			if err := cur.Err(); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(items)
+
+		case http.MethodPost:
+			// Create a new item
+			var item Item
+			if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			result, err := itemsCollection.InsertOne(context.Background(), item)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(result.InsertedID)
+
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 }
