4f51b29d8cdde484fc948eb81ecb165c6cac8999
ivandjoh <ivandi.djoh@gmail.com>
2023-02-20 22:46:51 +0800

    Create the Models with Structs

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Description
 This is a simple CRUD application using Go and MongoDB.
 
+`Golang, Gin Gonic, MongoDB CRUD RESTful API`
+
+
 ```
 Hold on, This Project will be updated soon!!
 ```
@@ -11,14 +14,3 @@ Hold on, This Project will be updated soon!!
 ```
 git clone https://github.com/ivandi1980/gomongo.git
 ```
-
-## Temporary Usage (for now and it will be updated soon)
-
-- Start the API by running `go run main.go`
-- Use a REST client like Postman or Insomnia to test the API. Here are some example requests:
-
-- GET all items: `GET http://localhost:8080/items`
-- GET a single item by ID: `GET http://localhost:8080/items/{id}`
-- POST a new item: `POST http://localhost:8080/items` with a JSON body like `{"name": "Item 1", "price": 10}`
-- PUT an existing item by ID: `PUT http://localhost:8080/items/{id}` with a JSON body like `{"name": "Updated Item 1", "price": 20}`
-- DELETE an item by ID: `DELETE http://localhost:8080/items/{id}`
---
+++ models/user.model.go
@@ -0,0 +1,82 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type SignUpInput struct {
+	Name               string    `json:"name" bson:"name" binding:"required"`
+	Email              string    `json:"email" bson:"email" binding:"required"`
+	Password           string    `json:"password" bson:"password" binding:"required,min=8"`
+	PasswordConfirm    string    `json:"passwordConfirm" bson:"passwordConfirm,omitempty" binding:"required"`
+	Role               string    `json:"role" bson:"role"`
+	VerificationCode   string    `json:"verificationCode,omitempty" bson:"verificationCode,omitempty"`
+	ResetPasswordToken string    `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	ResetPasswordAt    time.Time `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	Verified           bool      `json:"verified" bson:"verified"`
+	CreatedAt          time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt          time.Time `json:"updated_at" bson:"updated_at"`
+}
+
+type SignInInput struct {
+	Email    string `json:"email" bson:"email" binding:"required"`
+	Password string `json:"password" bson:"password" binding:"required"`
+}
+
+type UserDBResponse struct {
+	ID                 primitive.ObjectID `json:"id" bson:"_id"`
+	Name               string             `json:"name" bson:"name"`
+	Email              string             `json:"email" bson:"email"`
+	Password           string             `json:"password" bson:"password"`
+	PasswordConfirm    string             `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+	Role               string             `json:"role" bson:"role"`
+	VerificationCode   string             `json:"verificationCode,omitempty" bson:"verificationCode"`
+	ResetPasswordToken string             `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	ResetPasswordAt    time.Time          `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	Verified           bool               `json:"verified" bson:"verified"`
+	CreatedAt          time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt          time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+type UserUpdateInput struct {
+	Name               string    `json:"name,omitempty" bson:"name,omitempty"`
+	Email              string    `json:"email,omitempty" bson:"email,omitempty"`
+	Password           string    `json:"password,omitempty" bson:"password,omitempty"`
+	Role               string    `json:"role,omitempty" bson:"role,omitempty"`
+	VerificationCode   string    `json:"verificationCode,omitempty" bson:"verificationCode,omitempty"`
+	ResetPasswordToken string    `json:"resetPasswordToken,omitempty" bson:"resetPasswordToken,omitempty"`
+	ResetPasswordAt    time.Time `json:"resetPasswordAt,omitempty" bson:"resetPasswordAt,omitempty"`
+	Verified           bool      `json:"verified,omitempty" bson:"verified,omitempty"`
+	CreatedAt          time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt          time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
+
+type UserResponse struct {
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty"`
+	Role      string             `json:"role,omitempty" bson:"role,omitempty"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+type ForgotPasswordInput struct {
+	Email string `json:"email" bson:"email" binding:"required"`
+}
+
+type ResetPasswordInput struct {
+	Password        string `json:"password" bson:"password"`
+	PasswordConfirm string `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+}
+
+func FilteredUserResponse(user *UserDBResponse) UserResponse {
+	return UserResponse{
+		ID:        user.ID,
+		Email:     user.Email,
+		Name:      user.Name,
+		Role:      user.Role,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+}
